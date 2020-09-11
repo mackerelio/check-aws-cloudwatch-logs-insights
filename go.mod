e32302cb7f22c1ab54cb6fc58c3c5d7fa04e13f1
Asato Wakisaka <asato.wakisaka@gmail.com>
2020-09-11 20:01:23 +0900

    start checker

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hatena/mackerel-check-aws-cloudwatch-logs-insights
 
 go 1.15
+
+require github.com/mackerelio/checkers v0.0.0-20200428063449-52cfb2c2c52c
--- lib/app.go
+++ lib/app.go
@@ -1,12 +1,18 @@
 package checkawscloudwatchlogsinsights
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/mackerelio/checkers"
 )
 
 // Do the logic
 func Do() {
-	fmt.Println("hello")
-	os.Exit(0)
+	ckr := run(os.Args[1:])
+	ckr.Name = "CloudWatch Logs Insights"
+	ckr.Exit()
+}
+
+func run(args []string) *checkers.Checker {
+	return checkers.Ok("This is a test checker")
 }
